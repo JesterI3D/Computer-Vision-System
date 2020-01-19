58e45ee1412865d8581c29df5a08f1c0db5ef35b
Никита <43285442+JesterI3D@users.noreply.github.com>
2020-01-19 06:19:03 +0300

    Add files via upload

---
+++ unit-test.py
@@ -0,0 +1,13 @@
+import unittest
+import lanes
+import cv2
+import numpy as np
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main(lanes)
